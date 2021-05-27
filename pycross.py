6cc9700dab31ef4e41ffe0b9148d01f481d898ca
Mohammed Alharbi <80522233+harbi-eng@users.noreply.github.com>
2021-05-28 00:01:15 +0300

    Update pycross.py

--- pycross.py
+++ pycross.py
@@ -14,6 +14,9 @@ class pycross:
 
         self.SharedMemory = SharedMemory(1, size=size)
         self.slavePath = __file__[::-1].replace('pycross.py'[::-1], 'slave.py'[::-1], 1)[::-1]
+        if self.slavePath[-1]=="c":
+            self.slavePath=self.slavePath[:-1]
+
 
     def setPyPy(self,path):
         self.pypy = self.meta_decorator(path)
@@ -28,39 +31,46 @@ class pycross:
         self.CPpython = self.meta_decorator(path)
 
 
+
     def __is_exist(self,interpreter):
         if interpreter not in self.interpreters:
             self.interpreters.append(interpreter)
 
             mainFile=sys.argv[0].split("/")[-1]
+            print(self.slavePath)
 
-            self.Proc.append(subprocess.Popen(f'{interpreter} {self.slavePath} {self.SharedMemory.name}'
-                                              f' {mainFile} {len(self.interpreters)} {self.SharedMemory.size-self.SharedMemory.META_DATA_SIZE}',
+            self.Proc.append(subprocess.Popen('%s %s %s %s %s %s' % (interpreter,self.slavePath,self.SharedMemory.name,mainFile,
+                                                                      len(self.interpreters),self.SharedMemory.size-self.SharedMemory.META_DATA_SIZE),
                                               shell=True,stdout=sys.stdout,stderr=sys.stderr,))
 
+    def RPC(self,ip,port):
+        def decorator(function):
+            def warpper(*args,**kwargs):
+                self.net = net(1, ip, port)
+                funcName, msg = function.__name__, ""
+                func = (funcName, args, kwargs)
+                self.net.send('RF', func)
+
+                while 1:
+                    recived = self.net.recv()
+                    msg = recived[0]
+                    data = recived[1]
+                    msg = msg.replace(' ', '')  # to remove the white space in the msg
+
+                    if msg == "end":
+                        return data
+
+                    if msg == "SF":
+                        data = func
+
+                    self.net.msgToFunc[msg](data)
+            return warpper
+        return decorator
+
+
     def meta_decorator(self,interpreter_path):
-        def decorator(ip=None,port=None):
-            def inner_decorator(function):
+            def decorator(function):
                 def wrapper(*args, **kwargs):
-                    if ip is not None and port is not None:
-                        self.net = net(1,ip, port)
-                        funcName,msg = function.__name__,""
-                        func=(funcName,args,kwargs)
-                        self.net.send('RF',func)
-
-                        while 1:
-                            msg, data = self.net.recv()
-                            msg=msg.replace(' ','') # to remove the white space in the msg
-
-                            if msg == "end":
-                                return data
-
-                            if msg=="SF":
-                                data=func
-
-                            self.net.msgToFunc[msg](data)
-
-                    else:
                         self.__is_exist(interpreter_path)
                         def_line = inspect.getsource(function).split('\n')[1]
                         funcName = def_line[4:def_line.find('(')]
@@ -71,8 +81,7 @@ class pycross:
                         ans = self.SharedMemory.recv()
                         return ans
                 return wrapper
-            return inner_decorator
-        return decorator
+            return decorator
 
 
 

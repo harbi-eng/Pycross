29bb29cc8b8c54dc01cf85ce9f0273d6eaeefb1a
Mohammed Alharbi <80522233+harbi-eng@users.noreply.github.com>
2021-05-27 22:58:39 +0300

    Update std_override.py

--- std_override.py
+++ std_override.py
@@ -1,12 +1,14 @@
 import sys
-
 class output():
     def __init__(self,net):
         self.stdout=sys.stdout
         self.net = net
+        self.num=0
 
     def write(self, text):
-        self.net.sendOutput(text)
+        self.num+=1
+        if self.num%2:
+            self.net.sendOutput(text)
 
     def flush(self):
         self.stdout.flush()
